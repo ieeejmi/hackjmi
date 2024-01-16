f427e1281bbbfc713797833a2d46c41dea119739
TahirReyaz <tahirreyaz5@gmail.com>
2024-01-16 22:45:29 +0530

    Removed community parteners

--- src/data/components.data.js
+++ src/data/components.data.js
@@ -1,27 +1,26 @@
-import IEEEJHSB from "../assets/community-partners/IEEE-JHSB.jpg"
-import GDSCDTC from "../assets/community-partners/gdscdtc.jpeg"
-import GDSCSRCW from "../assets/community-partners/gdscsrcw.png"
-import Xplorica from "../assets/community-partners/xplorica.jpg"
-import Futureeducation from "../assets/community-partners/futureeducation.jpg"
-
+import IEEEJHSB from "../assets/community-partners/IEEE-JHSB.jpg";
+import GDSCDTC from "../assets/community-partners/gdscdtc.jpeg";
+import GDSCSRCW from "../assets/community-partners/gdscsrcw.png";
+import Xplorica from "../assets/community-partners/xplorica.jpg";
+import Futureeducation from "../assets/community-partners/futureeducation.jpg";
 
 const communitypartnerData = [
-    {
-        src: GDSCDTC
-    },
-    {
-        src: IEEEJHSB
-    },
-    {
-        src: Xplorica,
-        href: "http://xplorica.in/"
-    },
-    {
-        src: Futureeducation
-    },
-    {
-        src: GDSCSRCW
-    }
+  // {
+  //     src: GDSCDTC
+  // },
+  {
+    src: IEEEJHSB,
+  },
+  // {
+  //     src: Xplorica,
+  //     href: "http://xplorica.in/"
+  // },
+  // {
+  //     src: Futureeducation
+  // },
+  // {
+  //     src: GDSCSRCW
+  // }
 ];
 
 export default communitypartnerData;
